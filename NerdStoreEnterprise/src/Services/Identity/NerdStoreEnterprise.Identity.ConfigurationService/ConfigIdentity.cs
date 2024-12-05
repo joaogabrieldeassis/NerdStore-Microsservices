c4d1d14e87dc22898bc08c958f33483b21f40427
joaoambi <joaoassisgabriel@gmail.com>
2024-12-05 15:16:27 -0300

    Implementando o identity

---
+++ NerdStoreEnterprise/src/Services/Identity/NerdStoreEnterprise.Identity.ConfigurationService/ConfigIdentity.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using NerdStoreEnterprise.Identity.Api.Data;
+
+namespace NerdStoreEnterprise.Identity.ConfigurationService;
+
+public static class ConfigIdentity
+{
+    public static void ConfigureIdentityService(this IServiceCollection services, string connectionString)
+    {
+        services.AddDbContext<ApplicationDbContext>(options =>
+        options.UseSqlServer(connectionString));
+
+        services.AddDefaultIdentity<IdentityUser>()
+            .AddRoles<IdentityRole>()
+            .AddEntityFrameworkStores<ApplicationDbContext>()
+            .AddDefaultTokenProviders();
+    }
+}
