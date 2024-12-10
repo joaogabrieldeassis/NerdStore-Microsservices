de5a56010493763d6672f00546555a03f7f01ed5
joaoambi <joaoassisgabriel@gmail.com>
2024-12-10 15:45:33 -0300

    Ajuste no endpoint

--- NerdStoreEnterprise/src/Web/NerdStoreEntripese.WebApp.MVC/Services/AutenticationService.cs
+++ NerdStoreEnterprise/src/Web/NerdStoreEntripese.WebApp.MVC/Services/AutenticationService.cs
@@ -4,15 +4,16 @@ using System.Text.Json;
 
 namespace NerdStoreEntripese.WebApp.MVC.Services;
 
-public class AutenticationService(HttpClient httpClient) : Service, IAutenticationService
+public class AutenticationService(HttpClient httpClient, IConfiguration configuration) : Service, IAutenticationService
 {
     private readonly HttpClient _httpClient = httpClient;
+    private readonly string _urlApiIdentity = configuration.GetSection("Autentication").Value!;
 
     public async Task<ResponseRequest?> Login(Models.Users.LoginUser loginUser)
     {
         var optionsContext = GetContent(loginUser);
 
-        var response = await _httpClient.PostAsync("https://localhost:7225/Authentication/logar", optionsContext);
+        var response = await _httpClient.PostAsync($"{_urlApiIdentity}/logar", optionsContext);
 
         if (TreatErrosResponse(response))
             return await DeserializarObjetoResponse<ResponseRequest>(response);
@@ -24,7 +25,7 @@ public class AutenticationService(HttpClient httpClient) : Service, IAutenticati
     {
         var optionsContext = GetContent(registerUser);
 
-        var response = await _httpClient.PostAsync("https://localhost:7225/Authentication/registrar", optionsContext);
+        var response = await _httpClient.PostAsync($"{_urlApiIdentity}/registrar", optionsContext);
 
         if (TreatErrosResponse(response))
             return new ResponseRequest
@@ -32,7 +33,7 @@ public class AutenticationService(HttpClient httpClient) : Service, IAutenticati
                 Data = new UserResponseLogin
                 {
                     ResponseResult = await DeserializarObjetoResponse<ResponseResult>(response)
-                }                
+                }
             };
 
         return await DeserializarObjetoResponse<ResponseRequest>(response);
